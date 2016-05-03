56e7235814daf44887fbdfc771a663c4ab2c6938
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-03 13:04:55 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1258,7 +1258,7 @@ local function run(msg, matches)
 				resolve_username(username,  callbackres, cbres_extra)
 			else
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup ID")
-				return "> Group ID: "..msg.to.id.."\n> Group Name: "..msg.to.title.."\n> First Name: "..(msg.from.first_name or '').."\n> Last Name: "..(msg.from.last_name or '').."\n> Your ID: "..msg.from.id.."\n> Username: https://telegram.me/"..(msg.from.username or '').."\n> Phone Number: +"..(msg.from.phone or '')			end
+				return "🆔 ایدی گروه: "..msg.to.id.."\n♋️ اسم گروه: "..msg.to.title.."\n> 🔘 شما: "..(msg.from.first_name or '').."\n⚫️ نام خانوادگی: "..(msg.from.last_name or '').."\n💢 ایدی عددی شما: "..msg.from.id.."\n♒️ ایدی شما: https://telegram.me/"..(msg.from.username or '').."\n🔰 شماره تلفن: +"..(msg.from.phone or '')			end
 		end
 
 		if matches[1] == 'kickme' then
