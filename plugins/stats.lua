f7b16108c0674763a0f242c38d545027acefe478
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-03 21:55:02 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,7 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+  
     }, 
   run = run
 }
