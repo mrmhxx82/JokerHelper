8ec71e6694892d706ffa3a48d40081167bc5bc8f
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-04 14:20:41 +0430

    Update LinkPv.lua

--- plugins/LinkPv.lua
+++ plugins/LinkPv.lua
@@ -3,16 +3,16 @@ do
 
 function run(msg, matches)
        if not is_owner(msg) then
-        return "⛔️ شما دسترسی ندارید ⛔️"
+        return "شما دسترسی ندارید ⛔️"
        end
     local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
-        return "➕ اول با دستور /newlink یک لینک جدید بسازید"
+        return "اول با دستور /newlink یک لینک جدید بسازید ➕"
        end
          local text = "🔘 لینک سوپر گروه   :\n"..group_link
           send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false)
-           return "✅ لینک گروه در چت خصوصی ارسال شد"
+           return "لینک گروه در چت خصوصی ارسال شد ✅"
 end
 
 return {
