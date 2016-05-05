671b5e9f09823bd0240023bb5596eb5456b0fccc
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-05 22:50:20 +0430

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -261,7 +261,7 @@ end
 	if matches[1] == 'reload' then
 		receiver = get_receiver(msg)
 		reload_plugins(true)
-		post_msg(receiver, "ریلود شد !"", ok_cb, false)
+		post_msg(receiver, "Reloaded!", ok_cb, false)
 		return "ریلود شد !"
 	end
 	--[[*For Debug*
