18a0116edcfffa62dd3ec5ff8483449a3faa9be0
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-06 13:44:57 +0430

    Update music.lua

--- plugins/music.lua
+++ plugins/music.lua
@@ -4,7 +4,7 @@ local function musiclink(msg, musicid)
 	if not value then
 		return
 	else
-		value = value..'\n\nMrMH'
+		value = value..'\n\nبا تشکر از @gpmod'
 		return value
 	end
 end
@@ -37,7 +37,7 @@ function run(msg, matches)
 		if not value then
 			return 'آهنگ مورد نظر پیدا نشد.'
 		else
-			value = value..'\n\nMrMH'
+			value = value..'\n\nبا تشکر از @gpmod'
 			return value
 		end
 		return
