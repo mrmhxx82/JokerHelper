49e810e8621edba6c706bdb1bfeecc22316fb239
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-06 13:10:33 +0430

    Update anti_spam.lua

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -108,10 +108,10 @@ local function pre_process(msg)
 	  if msg.to.type == 'chat' or msg.to.type == 'channel' then
 		if username then
 			savelog(msg.to.id, name_log.." @"..username.." ["..msg.from.id.."] kicked for #spam")
-			send_large_msg(receiver , "Flooding is not allowed here\n@"..username.."["..msg.from.id.."]\nStatus: User kicked")
+			send_large_msg(receiver , "اسپم مجاز نمیباشد\n@"..username.."["..msg.from.id.."]\nوضعیت: اخراج از گروه")
 		else
 			savelog(msg.to.id, name_log.." ["..msg.from.id.."] kicked for #spam")
-			send_large_msg(receiver , "Flooding is not allowed here\nName:"..name_log.."["..msg.from.id.."]\nStatus: User kicked")
+			send_large_msg(receiver , "اسپم مجاز نمیباشد\n@"..username.."["..msg.from.id.."]\nوضعیت: اخراج از گروه")
 		end
 	  end
       -- incr it on redis
