7df8b95ca6c0f063ffd146887cad62d269ab3df3
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-06 13:59:13 +0430

    Update JokerBot.lua

--- plugins/JokerBot.lua
+++ plugins/JokerBot.lua
@@ -3,12 +3,12 @@ function run(msg, matches)
   return " جوکر بات محافط 24 ساعته ی گروه های شما \n\n توسعه دهنده: @Mr_Mh58 \n کانال: @jokerantichanel"
   end
 return {
-  description = " TeleIran ", 
-  usage = " TeleIran ",
+  description = " JokerBot ", 
+  usage = " JokerBot ",
   patterns = {
-    "^[#/!@][Jj][Oo][Kk][Rr][Bb][Oo][Tt]$",
-    "^[#/!@][Vv][Ee][Rr][Ss][Ii][Oo][Nn]$",
-    "^[#/!@][Jj][Oo][Kk][Rr]$",
+    "^[#/!][Jj][Oo][Kk][Rr][Bb][Oo][Tt]$",
+    "^[#/!][Vv][Ee][Rr][Ss][Ii][Oo][Nn]$",
+    "^[#/!][Jj][Oo][Kk][Rr]$",
   },
   run = run
 }
