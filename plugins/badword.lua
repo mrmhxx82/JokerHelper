7ea05978d6914bb5f749604d96caec7f8e9150e8
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-06 20:04:54 +0430

    Update badword.lua

--- plugins/badword.lua
+++ plugins/badword.lua
@@ -1,8 +1,15 @@
+--[[
 
+#
+#     @GPMOD
+#   @Dragon_Born
+#      
+
+]]
 local function addword(msg, name)
     local hash = 'chat:'..msg.to.id..':badword'
     redis:hset(hash, name, 'newword')
-    return "A new word was added to the filter words\n>"..name
+    return "کلمه جدید به فیلتر کلمات اضافه شد\n>"..name
 end
 
 local function get_variables_hash(msg)
@@ -16,13 +23,13 @@ local function list_variablesbad(msg)
 
   if hash then
     local names = redis:hkeys(hash)
-    local text = 'Bad words list :\n\n'
+    local text = 'لیست کلمات غیرمجاز :\n\n'
     for i=1, #names do
       text = text..'> '..names[i]..'\n'
     end
     return text
-	else
-	return 
+ else
+ return 
   end
 end
 
@@ -30,7 +37,7 @@ function clear_commandbad(msg, var_name)
   --Save on redis  
   local hash = get_variables_hash(msg)
   redis:del(hash, var_name)
-  return 'Been deleted'
+  return 'پاک شدند'
 end
 
 local function list_variables2(msg, value)
@@ -40,13 +47,13 @@ local function list_variables2(msg, value)
     local names = redis:hkeys(hash)
     local text = ''
     for i=1, #names do
-	if string.match(value, names[i]) and not is_momod(msg) then
-	if msg.to.type == 'channel' then
-	delete_msg(msg.id,ok_cb,false)
-	else
-	kick_user(msg.from.id, msg.to.id)
+ if string.match(value, names[i]) and not is_momod(msg) then
+ if msg.to.type == 'channel' then
+ delete_msg(msg.id,ok_cb,false)
+ else
+ kick_user(msg.from.id, msg.to.id)
 
-	end
+ end
 return 
 end
       --text = text..names[i]..'\n'
@@ -68,11 +75,11 @@ function clear_commandsbad(msg, cmd_name)
   --Save on redis  
   local hash = get_variables_hash(msg)
   redis:hdel(hash, cmd_name)
-  return ''..cmd_name..'  Cleared'
+  return ''..cmd_name..'  پاک شد'
 end
 
 local function run(msg, matches)
-  if matches[2] == 'blockword' then
+  if matches[2] == 'addword' then
   if not is_momod(msg) then
    return 'only for moderators'
   end
@@ -81,13 +88,13 @@ local function run(msg, matches)
   local text = addword(msg, name)
   return text
   end
-  if matches[2] == 'blockwordlist' then
+  if matches[2] == 'badwords' then
   return list_variablesbad(msg)
-  elseif matches[2] == 'unblockallword' then
+  elseif matches[2] == 'clearbadwords' then
 if not is_momod(msg) then return '_|_' end
   local asd = '1'
     return clear_commandbad(msg, asd)
-  elseif matches[2] == 'unblockword' or matches[2] == 'unblockword' then
+  elseif matches[2] == 'remword' or matches[2] == 'rw' then
    if not is_momod(msg) then return '_|_' end
     return clear_commandsbad(msg, matches[3])
   else
@@ -99,13 +106,13 @@ end
 
 return {
   patterns = {
-  "^([#!/])(unblockword) (.*)$",
-  "^([#!/])(blockword) (.*)$",
-   "^([#!/])(unblockword) (.*)$",
-    "^([#!/])(blockwordlist)$",
-    "^([#!/])(unblockallword)$",
+  "^([!/])(rw) (.*)$",
+  "^([!/])(addword) (.*)$",
+   "^([!/])(remword) (.*)$",
+    "^([!/])(badwords)$",
+    "^([!/])(clearbadwords)$",
 "^(.+)$",
-	   
+    
   },
   run = run
 }
