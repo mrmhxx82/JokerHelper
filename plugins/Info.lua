5768a0338d5d62567b19d73e22d9d0d8ee11f125
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-07 19:38:14 +0430

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -1,6 +1,6 @@
 do
 function run(msg, matches)
-  return "🛡#نام_گروه > "..msg.to.title.."\n🕵#نام_شما > "..(msg.from.first_name or ").."\n👤#نام_اول > "..(msg.from.first_name or ").."\n👥#نام_آخر > "..(msg.from.last_name or ").."\n👾#آیدی > "..msg.from.id.."\n💡#یوزرنیم > @"..(msg.from.username or ").."\n📱#شمار_تلفن > "..(msg.from.phone or ").."+".."\n😐#ایدی_پیام_تو > "..msg.id.."\n😏#نام_کامل > "..(msg.from.print_name or ").."\n#نوع سیمکارت > "..(msg.from.sim or ").."\n #زمان > "..(msg.from.loc or ").."\ncreater:@sorblack (͡• ͜ʖ ͡•)\nchannel: @PrimeTeam"
+  return "🛡#نام_گروه > "..msg.to.title.."\n🕵#نام_شما > "..(msg.from.first_name or ").."\n👤#نام_اول > "..(msg.from.first_name or ").."\n👥#نام_آخر > "..(msg.from.last_name or ").."\n👾#آیدی > "..msg.from.id.."\n💡#یوزرنیم > @"..(msg.from.username or ").."\n📱#شمار_تلفن > "..(msg.from.phone or ").."+".."\n😐#ایدی_پیام_تو > "..msg.id.."\n😏#نام_کامل > "..(msg.from.print_name or ").."\n#نوع سیمکارت > "..(msg.from.sim or ").."\n #زمان > "..(msg.from.loc or ").."\n@jokeranti"
 end
 return {
   description = "", 
