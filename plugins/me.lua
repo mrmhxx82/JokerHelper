2ef45fe7e0fdb2d70de36719edea71485a3b43af
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-07 21:27:58 +0430

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -4,19 +4,19 @@ local function run(msg, matches)
   if matches[1] == 'me' then
     if is_sudo(msg) then
 	  send_document(get_receiver(msg), "./files/me/sudo.webp", ok_cb, false)
-      return "You're sudo!"
+      return "🙃 شما سازنده هستید"
     elseif is_admin(msg) then
 	  send_document(get_receiver(msg), "./files/me/admin.webp", ok_cb, false)
-      return "You're admin!"
+      return "🙃 شما ادمین هستید"
     elseif is_owner(msg) then
 	  send_document(get_receiver(msg), "./files/me/leader.webp", ok_cb, false)
       return "You're the Leader!"
     elseif is_momod(msg) then
 	  send_document(get_receiver(msg), "./files/me/moderator.webp", ok_cb, false)
-      return "You're a moderator!"
+      return "🙃 شما مدیر هستید"
     else
 	  send_document(get_receiver(msg), "./files/me/member.webp", ok_cb, false)
-      return "You're just a member!"
+      return "🙃 شما ممبر هستید"
     end
   end
 end
