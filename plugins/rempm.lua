aa8fe57360a3a0188e715d5f05bcdd9df38ce97c
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-08 17:16:37 +0430

    Update rempm.lua

--- plugins/rempm.lua
+++ plugins/rempm.lua
@@ -1,29 +1,30 @@
 local function history(extra, suc, result)
-  for i=1, #result do
-    delete_msg(result[i].id, ok_cb, false)
-  end
-  if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'"✅ پیام اخیر سوپر گروه حذف شد ✅', 
-ok_cb, false)
-  else
-    send_msg(extra.chatid, '✅ تعداد پیام مورد نظر شما پاک شد ✅', ok_cb, 
-false)
-  end end local function run(msg, matches)
-  if matches[1] == 'rm' and is_owner(msg) then
-    if msg.to.type == 'channel' then
-      if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
-        return "❗️ تعداد کم تر از 1 مجاز نیست ❗️"
-      end
-      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = 
-msg.to.peer_id, con = matches[2]})
-    else
-      return "🌀 فقط در سوپرگروه ممکن است 🌀"
-    end
-  else
-    return "⛔️ شما دسترسی ندارید ⛔️"
-  end end return {
+for i=1, #result do
+delete_msg(result[i].id, ok_cb, false)
+end
+if tonumber(extra.con) == #result then
+send_msg(extra.chatid, ''..#result..' پیام اخیر سوپر گروه حذف شد', ok_cb, false)
+else
+send_msg(extra.chatid, 'تمام پیام های سوپر گروه حذف شد', ok_cb, false)
+end
+end
+local function run(msg, matches)
+if matches[1] == 'msgrem' and is_owner(msg) then
+            if msg.to.type == 'channel' then
+            if tonumber(matches[2]) > 99 or tonumber(matches[2]) < 1 then
+            return "خطا عدد انتخاب شده باید زیر 100 باشد"
+            end
+            get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+        else
+                         return "فقط مخصوص سوپرگروه می باشد"
+        end
+else
+return "فقط مخصوص مدیران گروه می باشد"
+end
+end
+return {
     patterns = {
-        '^[!/#](rm) (%d*)$'
+        '^[!/#](msgrem) (%d*)$'
     },
     run = run
 }
