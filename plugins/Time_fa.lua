8caa2ce769929142f6e09cc34680e708b362155e
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-12 10:10:58 -0700

    Update Time_fa.lua

--- plugins/Time_fa.lua
+++ plugins/Time_fa.lua
@@ -1,15 +1,11 @@
-
-
 function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '🕒 Ir Time:'..jdat.FAtime..' \n📆 Ir Data:'..jdat.FAdate..'\n    ------------\n🕒 En Time:'..jdat.ENtime..'\n📆 En Data:'..jdat.ENdate.. '\⚠ Chanel Id: @JokerTeam'
+local text = '🕒 ساعت '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n    ----\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate..'\n⚠ Chanel Id: @JokerTeam'
 return text
 end
 return {
-  patterns = {"^(زمان)$"}, 
+  patterns = {"^[/!]([Tt][iI][Mm][Ee])$"}, 
 run = run 
 }
-
-
