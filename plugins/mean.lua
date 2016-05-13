1f9145cefee92418fed490de9fd2513f6fa5ec03
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-13 11:49:26 +0430

    Update mean.lua

--- plugins/mean.lua
+++ plugins/mean.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
   local htp = http.request('http://api.vajehyab.com/v2/public/?q='..URL.escape(matches[1]))
   local data = json:decode(htp)
-	return 'کلمه : '..(data.data.title or data.search.q)..'\n\nمعنی : '..(data.data.text or '----' )..'\n\nمنبع : '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..''
+	return '> Word : '..(data.data.title or data.search.q)..'\n\n> Meaning : '..(data.data.text or '----' )..'\n\nSource : '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..'⚠️ Chanel Id : @JokerTeam'
 end
 return {
   patterns = {
