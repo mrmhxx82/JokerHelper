d0e8db6e3783eb9123c7db5005f9bb3aaa737a01
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-17 13:39:44 +0430

    Update Id.lua

--- plugins/Id.lua
+++ plugins/Id.lua
@@ -3,7 +3,7 @@ do
 local function run(msg, matches)
   if matches[1] == 'id' then
 	return "> Group ID: "..msg.to.id.."\n> Group Name: "..msg.to.title.."\n> First Name: "..(msg.from.first_name or '').."\n> Last Name: "..(msg.from.last_name or '').."\n> Your ID: "..msg.from.id.."\n> Username: @"..(msg.from.username or '').."\n> Phone Number: +"..(msg.from.phone or '') endend
-
+	end
 return {
   patterns = {
     "^[!#$/]([Ii]d)$",
