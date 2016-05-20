2eed875cec2af550864361fe09891b8a0fcb706c
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-20 22:09:29 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -29,7 +29,7 @@ local function check_member_super(cb_extra, success, result)
 		  lock_tgservice = 'yes',
 		  lock_contacts = 'yes',
 		  strict = 'no'
-		  lock_fosh = 'yes'
+		 
         }
       }
       save_data(_config.moderation.data, data)
