7689911434f00293b7dda5ac0c11ba09fceaf0b9
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-21 19:02:37 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -4,7 +4,8 @@ function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '🕒 Ir Time:'..jdat.FAtime..' \n📆 Ir Data:'..jdat.FAdate..'\n    ------------\n🕒 En Time:'..jdat.ENtime..'\n📆 En Data:'..jdat.ENdate.. '\n⚠ Chanel Id: @JokerTeam'
+local text = '🕒 Ir Time: '..jdat.FAtime..' \n📆 Ir Data: '..jdat.FAdate..'\n---------------------------------\n🕒 En Time: '..jdat.ENtime..'\n📆 En Data :'..jdat.ENdate.. '\n---------------------------------
+\n🔰 Chanel Id: @FuckSon_Ch'
 return text
 end
 return {
